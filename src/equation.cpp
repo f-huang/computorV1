9bb01bc2f52fdae9ba189b7fa5c6c1fdfc92faa7
Fanny HUANG <fhuang@e3r5p18.42.fr>
2017-12-08 18:24:50 +0100

    Regex for parsing

--- src/equation.cpp
+++ src/equation.cpp
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 16:47:53 by fhuang            #+#    #+#             */
-/*   Updated: 2017/12/07 12:34:44 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/12/08 18:24:27 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,63 @@
 #include <string>
 #include <regex>
 #include "equation.h"
-using namespace std;
+
+#define REGEX_PATTERN1 "([xX] *(\\^ *[0-2])? *(\\* *\\d)?|\\d *\\* *[xX] *(\\^ *[0-2])?|\\d|\\d *[xX] *(\\^ *[0-2])?) *([+-=]|$)"
 
 equation::equation(const char *av)
 {
+	std::string::iterator	end;
+
 	str = av;
+	end = std::remove(str.begin(), str.end(), ' ');
+	str.erase(end, str.end());
 }
 
 bool	equation::parse()
 {
-	regex	pattern("([xX] *\^ *[0-2] *\* *\d|\d *\* *[xX] *(\^ *[0-2])*|\d) *[+-=]");
+	std::regex	pattern1(REGEX_PATTERN1);
 
+	std::regex_iterator<std::string::iterator>	iterator(str.begin(), str.end(), pattern1);
+	std::regex_iterator<std::string::iterator>	end;
+	size_t		len;
 
-	return true;
+	len = 0;
+	std::cout << "Total = " << std::distance(iterator, end) << std::endl;
+	while (iterator != end)
+	{
+		// std::cout << iterator->str() << std::endl;
+		len += iterator->str().length();
+		++iterator;
+	}
+
+	return (len == str.length());
 }
 
 bool	equation::isCorrect()
 {
-	if (str.find('=') == string::npos)
-		return false;
-	return true;
+	if (str.find('=') == std::string::npos)
+		return (false);
+	return (equation::parse());
 }
 
 /*
+TEST :
+2 = 0
+2 + 2 = 0
 
-	5 * X^0 + 4 * X^1 + 8 * X^2 = 0
-	5 * X^0 * 4 * X^1 + 8 * X^2 = 0
-	5 + 4 * X^1 + 8 * X^2 = 0
-	5 + 4 * X + 8 * X^2 = 0
+x+2 = 0
+x^1 + 2 = 0
+5x+2 = 0
+5 * x + 2 = 0
+5 * 3x + 2 = 0
+5 * x^1 + 2 = 0
+5 * 4x^1 + 2 = 0
+5 * x^1 + 2x^2 = 0
+
+2 + x = 0
+2 + x^1 = 0
+2 + 5x = 0
+2 + 5*x = 0
+2 + 5*x + 2 = 0
+2 + 5*x ^1 = 0
 */
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/27 14:01:05 by fhuang            #+#    #+#             */
-/*   Updated: 2017/12/07 12:34:17 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/12/08 15:08:24 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 #include <string>
 #include "computor.h"
 #include "equation.h"
-using namespace std;
 
-#define ERROR_FORMAT	PROGRAM_NAME": Not well formatted"
-#define ERROR_USAGE PROGRAM_NAME": illegal arguments\nusage: computor <equation>"
+#define ERROR_FORMAT PROGRAM_NAME": Not well formatted"
+#define ERROR_USAGE  PROGRAM_NAME": illegal arguments\nusage: computor <equation>"
 
 int		main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		cerr << ERROR_USAGE << endl;
+		std::cerr << ERROR_USAGE << std::endl;
 		return (1);
 	}
 
 	equation	equation(av[1]);
 	if (!equation.isCorrect())
 	{
-		cerr << ERROR_FORMAT << endl;
+		std::cerr << ERROR_FORMAT << std::endl;
 		return (1);
 	}
 	return (0);
